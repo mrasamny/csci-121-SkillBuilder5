056921cc61f3d89cb848a1da4654c14a52e55690
mrasamny <mrasamny@desu.edu>
2023-11-08 20:37:02 -0500

    First Commit

--- src/test/java/SkillBuilder5Test.java
+++ src/test/java/SkillBuilder5Test.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SkillBuilder5Test {
 
     @Test
+    @DisplayName("[1] Test prefixAverage_1")
     void prefixAverage_1() {
         double[] a = {8.3,23,45.4,98,3.2,8.8,9.9,0.4};
         double[] expected = {8.3,15.65,25.56666667,43.675,35.58,31.1166667,28.0857143,24.625};
@@ -13,6 +15,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[1] Test prefixAverage_2")
     void prefixAverage_2() {
         double[] a = {1,100,1000,10000};
         double[] expected = {1.0,50.5,367.0,2775.25};
@@ -21,6 +24,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[1] Test prefixAverage_3")
     void prefixAverage_3() {
         double[] a = {1};
         double[] expected = {1};
@@ -29,6 +33,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[1] Test indexOf_1")
     void indexOf_1() {
         int[] a = {88,15,34,8, 34,89,55,12};
         int expected = 2;
@@ -38,6 +43,7 @@ class SkillBuilder5Test {
 
 
     @Test
+    @DisplayName("[1] Test indexOf_2")
     void indexOf_2() {
         int[] a = {88,15,34,8, 34,89,55,12};
         int expected = 0;
@@ -46,6 +52,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[1] Test indexOf_3")
     void indexOf_3() {
         int[] a = {88,15,34,8, 34,89,55,12};
         int expected = -1;
@@ -54,6 +61,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[1] Test IndexOfStrings_1")
     void IndexOfStrings_1() {
         String[] a = {"help","me","test","this","method","help"};
         int expected = 0;
@@ -62,6 +70,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[1] Test IndexOfStrings_2")
     void IndexOfStrings_2() {
         String[] a = {"help","me","test","this","method","help"};
         int expected = 3;
@@ -70,6 +79,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[1] Test IndexOfStrings_3")
     void IndexOfStrings_3() {
         String[] a = {"help","me","test","this","method","help"};
         int expected = -1;
@@ -78,6 +88,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[2] Test remove_1")
     void remove_1() {
         String[] a = {"help","me","test","this","method","help"};
         String[] expected = {"help","me","test","method","help"};
@@ -86,6 +97,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[2] Test remove_2")
     void remove_2() {
         String[] a = {"help","me","test","this","method"};
         String[] expected = {"help","me","test","this"};
@@ -94,6 +106,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[2] Test remove_3")
     void remove_3() {
         String[] a = {"help"};
         String[] expected = {};
@@ -102,6 +115,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[2] Test remove_4")
     void remove_4() {
         String[] a = {"help","me","test","this","method"};
         String[] expected = {"me","test","this", "method"};
@@ -110,6 +124,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[2] Test remove_5")
     void remove_5() {
         String[] a = {"test", "help","me","test","this","method", "this", "test", "should", "pass"};
         String[] expected = {"help","me", "this", "method", "this", "should", "pass"};
@@ -118,6 +133,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[3] Test reverse_1")
     void reverse_1() {
         int[] a = {88,15,34,8,34,89,55,12};
         int[] expected = {12,55,89,34,8,34,15,88};
@@ -126,6 +142,7 @@ class SkillBuilder5Test {
     }
 
     @Test
+    @DisplayName("[3] Test reverse_1")
     void reverse_2() {
         int[] a = {88};
         int[] expected = {88};
